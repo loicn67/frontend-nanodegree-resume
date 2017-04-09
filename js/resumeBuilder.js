ee1a9846aaaac2a50e680a91b8acfc3dbd2b2e26
lnonnenmacher <loicn67@users.noreply.github.com>
2017-04-09 21:15:13 +0100

    Add formatted name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,16 @@
-$("#main").append("Loic Nonnenmacher");
+
+var awesomeThoughts = "I am Loic and I am AWESOME!";
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+console.log(awesomeThoughts);
+console.log(funThoughts);
+
+var name = "Loic Nonnenmacher";
+var role = "Web Developper";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
